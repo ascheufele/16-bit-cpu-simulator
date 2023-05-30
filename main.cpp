468834950f53d30e2be0d63560e41928069d729e
Alex Scheufele <49530147+ascheufele@users.noreply.github.com>
2023-05-30 12:55:11 -0400

    remove deprecated functions

--- main.cpp
+++ main.cpp
@@ -15,46 +15,13 @@
 #include "control_logic.hpp";
 #include "CPU.hpp";
 
-// notes
-// probably want to take this approach when optimizing:
-// everything inherits from component
-// we have a vector of components that we index based on microcode bit / column name
-// when we do this we can set in / out since we are in a polymorphic state
-
-// bus contents |= source
-
-void load_microcode()
-{
-	std::ifstream mc("./microcode.txt");
-	char c;
-	while (mc)
-	{	
-		for (int i = 0; i < MICROCODE_WIDTH; ++i)
-		{
-			mc >> c;
-			std::cout << c;
-		}
-		std::cout << std::endl;
-	}
-}
-
-void load_program()
-{
-	std::cout << "load program not yet implemented" << std::endl;
-}
-
 int main(void)
 {
 	std::system("python ./mTranslator.py"); // update microcode
 	std::system("python ./assembler.py myProgram.zander"); // update program binary
 	sf::RenderWindow win(sf::VideoMode(1280, 720), "CPU Simulator");
-	//Clock clock;
-	//Register A, B;
-	//Program_Counter programCounter;
 
 	// set up
-	//load_program();
-	//load_microcode();
 	CPU cpu("./myProgram.zander.binary");
 	
 	// sfml testing
